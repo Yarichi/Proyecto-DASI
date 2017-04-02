ccbdc11f8d609a28d3253917e2724cdc8d786de0
luis <luis@luis-laptop>
2017-04-02 11:04:01 +0100

    Calculo de rutas en Python Terminado y Testeado
    Se ha terminado el algoritmo del calculo de rutas en Python. Se ha
    testeado el funcionamiento y el rendimiento. Para mapas
    grandes(1000x1000) no funciona todo lo bien que se esperaba. Para mapas
    pequeños(100x100) el resultado es instantáneo incluso habiendo
    obstáculos.

--- ICAROMalmo/src/comunicacionPython/python/rutasLee.py
+++ ICAROMalmo/src/comunicacionPython/python/rutasLee.py
@@ -1,39 +1,40 @@
-import Queue.Queue
+from Queue import Queue
 
 class CalculoRutas:
 	
 	def __init__(self,(xIni,yIni),obstacles, w, h):
-		self.obstaculos = obstacles
-		self.xInicial = xInicial
+		self.obstaculos = obstacles #Inicialmente vamos a pensarlo como una lista de tuplas (x,y)
+		self.xInicial = xIni
 		self.yInicial = yIni
-		self.cola = Queue.Queue()
-		self.grid = {}
+		self.cola = Queue()
+		self.grid = {} # Doble diccionario indexando primero por la x (fila)
 		self.width = w
 		self.height = h
-
-	def generarPuntos(self,(x,y)):
-		#Comprobaciones de los limites del mapa
-		for i in [-1,1]:
-			if x+i >= 0 and x+1 < self.width:
-				self.grid["%i"%(x+i)]["%i"%(y)] = self.grid["%i"%(x)]["%i"%(y)]+1;
-				self.cola.put((x+i,y))
-		for i in [-1,1]:
-			if y+i >= 0 and y+1 < self.height:
-				if not ((y+j) in self.grid["%i"%(x)].keys()):
-					self.grid["%i"%(x)] = {}
-				self.grid["%i"%(x)]["%i"%(y+i)] = self.grid["%i"%(x)]["%i"%(y)]+1;
-				self.cola.put((x,y+i))
 		
 	def calculaRuta(self,dx,dy):
 		self.cola = Queue()
-		self.cola.put((x,y))
-		while not cola.empty():
+		self.cola.put((self.xInicial,self.yInicial))
+		self.grid["%i"%(self.xInicial)] = {}
+		self.grid["%i"%(self.xInicial)]["%i"%(self.yInicial)] = 0
+		while not self.cola.empty():
 			coords = self.cola.get()
 			if coords[0] == dx and coords[1] == dy:
 				return self.generaRutaEncontrada((dx,dy))
 			else:
 				self.generarPuntos(coords)
 		return None
+	def generarPuntos(self,(x,y)):
+		#Comprobaciones de los limites del mapa
+		for i in [-1,1]:
+			if x+i >= 0 and x+i < self.width and not self.inGrid(x+i, y) and not self.isAObstacle((x+i,y)):
+				if "%i"%(x+i) not in self.grid.keys():
+					self.grid["%i"%(x+i)] = {}
+				self.grid["%i"%(x+i)]["%i"%(y)] = self.grid["%i"%(x)]["%i"%(y)] + 1;
+				self.cola.put((x+i,y))
+		for i in [-1,1]:
+			if y+i >= 0 and y+i < self.height and not self.inGrid(x, y+i) and not self.isAObstacle((x,y+i)):
+				self.grid["%i"%(x)]["%i"%(y+i)] = self.grid["%i"%(x)]["%i"%(y)]+1;
+				self.cola.put((x,y+i))
 	
 	def inGrid(self, x, y):
 		keys = self.grid.keys()
@@ -45,20 +46,34 @@ class CalculoRutas:
 		miny = auxCoor[1]
 		x = auxCoor[0]
 		y = auxCoor[1]
-		for i in [-1,1]:
-			for j in [-1,1]:
+		for i in [-1,0,1]:
+			for j in [-1,0,1]:
 				if x+i >=0 and y+j>=0 and x+i<self.width and y+j<self.height and self.inGrid(x+i, y+j):
-					if self.grid["%i"%(x+i)]["%i"%(y+j)] >-1 and self.grid["%i"%(x+i)]["i"%(y+j)] < this.grid["%i"%(minx)]["%i"%miny]:
+					if self.grid["%i"%(x+i)]["%i"%(y+j)] >-1 and self.grid["%i"%(x+i)]["%i"%(y+j)] < self.grid["%i"%(minx)]["%i"%miny]:
 						minx = x+i
 						miny = y+j
 		return (minx,miny)
 	
-	def generaRutaEncontrada(self):
-		pass
+	def generaRutaEncontrada(self,(x,y)):
+		ruta = []
+		aux = (x,y)
+		while(x != self.xInicial or y != self.yInicial):
+			x = aux[0]
+			y = aux[1]
+			ruta.insert(0, (x,y))
+			aux = self.getMinimoAlrededor(aux)
+		return ruta
+	def isAObstacle(self,coor):
+		return coor in self.obstaculos
 		
-		
-		
-		
+
+
+
+if __name__ == "__main__":
+	c = CalculoRutas((99,99),[(2,3),(3,3),(3,2)],100,100)
+	print c.calculaRuta(1, 1)
+	
+'''		
 public class AlgoritmoRutaLee {
 
 
@@ -230,3 +245,5 @@ public class AlgoritmoRutaLee {
 			e.printStackTrace();
 		}
 	}
+	
+	'''
