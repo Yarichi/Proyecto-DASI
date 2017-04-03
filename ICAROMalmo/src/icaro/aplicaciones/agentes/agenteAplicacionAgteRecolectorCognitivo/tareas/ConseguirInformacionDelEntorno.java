5bc1ba671acf60bcf6199e2c7ae6ccd51bba1003
Sergio Moreno <sergiomoreno.dp@gmail.com>
2017-04-03 17:50:00 +0200

    Se ha puesto las cosas hechas

---
+++ ICAROMalmo/src/icaro/aplicaciones/agentes/agenteAplicacionAgteRecolectorCognitivo/tareas/ConseguirInformacionDelEntorno.java
@@ -0,0 +1,12 @@
+package icaro.aplicaciones.agentes.agenteAplicacionAgteRecolectorCognitivo.tareas;
+
+import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.TareaSincrona;
+
+public class ConseguirInformacionDelEntorno extends TareaSincrona{
+
+	@Override
+	public void ejecutar(Object... params) {
+		
+	}
+
+}
