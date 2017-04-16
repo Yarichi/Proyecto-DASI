3dc54098566506df7cfe7470006912e8c50ad5e0
Luis García <luigiter@hotmail.com>
2017-04-16 20:58:32 +0200

    Completado el esquema de reglas

---
+++ ICAROMalmo/src/icaro/aplicaciones/agentes/agenteAplicacionAgteRecolectorCognitivo/tareas/ConsecucionObjetivoRecoleccion.java
@@ -0,0 +1,13 @@
+package icaro.aplicaciones.agentes.agenteAplicacionAgteRecolectorCognitivo.tareas;
+
+import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.TareaSincrona;
+
+public class ConsecucionObjetivoRecoleccion extends TareaSincrona{
+
+	@Override
+	public void ejecutar(Object... params) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ ICAROMalmo/src/icaro/aplicaciones/agentes/agenteAplicacionAgteRecolectorCognitivo/tareas/PeticionRecolectarManzana.java
@@ -0,0 +1,13 @@
+package icaro.aplicaciones.agentes.agenteAplicacionAgteRecolectorCognitivo.tareas;
+
+import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.TareaSincrona;
+
+public class PeticionRecolectarManzana extends TareaSincrona{
+
+	@Override
+	public void ejecutar(Object... params) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
